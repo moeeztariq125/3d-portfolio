a635dbc6eb2935e0ceed4144ec3240e4f238fe6d
moeez tariq <moeeztariq125@gmail.com>
2023-09-29 13:01:44 +0000

    add: navbar transition

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,57 @@
 import './Navbar.scss'
 import Icon from './Icon'
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 const Navbar = () => {
   let icons = ['icon1','icon2','icon3','icon4']
-  let [isActive, setIsActive] = useState('icon1')
+  const [isActive, setIsActive] = useState('icon1')
+  const [coverStyle, setCoverStyles] = useState({})
+  const activeIconRef = useRef(null)
+
+  const updateCoverPosition = () => {
+    if (activeIconRef.current) {
+      const icon = activeIconRef.current.getBoundingClientRect();
+      const rootFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize); // Get the root font size in pixels
+
+      // Calculate values in rem units
+      setCoverStyles({
+        left: `${(icon.left / rootFontSize)}rem`,
+        top: `${(icon.top / rootFontSize)}rem`,
+        width: `${(icon.width / rootFontSize)}rem`,
+        height: `${(icon.height / rootFontSize)}rem`,
+      });
+    }
+  };
+
+  useEffect(() => {
+    // Initial position calculation
+    updateCoverPosition();
+
+    // Add a resize event listener to update position when the window size changes
+    window.addEventListener('resize', updateCoverPosition);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', updateCoverPosition);
+    };
+  }, [isActive]);
+  
   return (
-    <nav className='navbar'>
+    <>
+    <div className="icon__cover" style={coverStyle}/>
+    <nav className='navbar' >
     {icons.map((icon) => (
-        <Icon key={icon} isActive={isActive === icon} Name={icon} onClick = {()=>{setIsActive(icon)}}/>
+      <Icon key={icon}
+      isActive={isActive === icon} 
+      Name={icon} 
+      onClick = {()=>{setIsActive(icon)}} 
+      ref={(ref:any) => {
+        if(isActive === icon){activeIconRef.current = ref}
+      }}
+      />
+      
       ))}
     </nav>
+      </>
   )
 }
 
