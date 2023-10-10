a878d46325a9dbf182204b48e291f1002456f8f7
moeeztariq125 <moeeztariq125@gmail.com>
2023-10-10 21:09:34 +0000

    setup header content

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+import app_header_skills from './app_header'
+
+export {app_header_skills}
--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,12 +1,20 @@
+import {useState} from 'react'
 import { Line } from '../../components'
 import './Header.scss'
+import {app_header_skills} from '../../constants'
 const Header = () => {
+  const [word, setWord] = useState(app_header_skills[0])
+
   return (
     <div className='app__header'>
       <Line start={true}/>
       <div className='circle'/>
-      <p>sfaefe</p>
+      <div className='app__header__content'>
+      <h2>&lt;Start /&gt;</h2>
+      <h1>Hi, I'm <span className='title'>Moeez Tariq</span></h1>
+      <h1 className='before_skills'>I <span className='italic'>design</span> and <span className='bold'>develop</span> {word} </h1>
       </div>
+    </div>
   )
 }
 
